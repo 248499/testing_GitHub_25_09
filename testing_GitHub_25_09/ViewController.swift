68b85eae256c3fb2f0e23f31810c1a2c0c06901a
Abdullah Alnajjar <abdullahalnajjar89@icloud.com>
2018-09-25 10:37:03 +0300

    Initial Commit

---
+++ testing_GitHub_25_09/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing_GitHub_25_09
+//
+//  Created by Abdullah on 9/25/18.
+//  Copyright © 2018 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
